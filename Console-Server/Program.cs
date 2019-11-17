7a32c99a1fe845293ad566495cf2d635c1127ff3
Мокин Никита <mokin98@outlook.com>
2019-11-17 20:01:34 +0500

    Version.1.0

---
+++ Console-Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Console_Server
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any,904));
+            socket.Listen(5);//Слушает подключения. Максимальное количество может обработать.
+           Socket client = socket.Accept();//Принятие
+            Console.WriteLine("Новое подключение");
+            byte[] buffer = new byte[1024];
+            client.Receive(buffer);//Получаем от подкюченного сокета данные
+            Console.WriteLine(Encoding.ASCII.GetString(buffer));//Вывод данных на экран
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Console-client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);// Класс Socket для сетевого взаимодействия
+        /*AddressFamily: возвращает все адреса, используемые сокетом, InterNetwork: адрес по протоколу IPv4
+         * SocketType: возвращает тип сокета, Stream: обеспечивает надежную двустороннюю передачу данныx
+         * ProtocolType: возвращает одно из значений перечисления*/
+
+        static void Main (string[] args)
+        {
+            socket.Connect("127.0.0.1", 904);//Подключение к серверу
+            string message = Console.ReadLine();//Считываем сообщение пользователя
+            byte[] buffer = Encoding.ASCII.GetBytes(message);//Кодировка массива байтов
+            socket.Send(buffer);//Отправка
+            Console.ReadLine();
+
+        }
+    }
+ 
+}
